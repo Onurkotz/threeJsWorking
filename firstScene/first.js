a4ca563bff2d395f5b83b467b821ba2f58abd249
onur koc <onurkottz@gmail.com>
2022-05-30 16:04:23 +0300

    cube and triangle

--- firstScene/first.js
+++ firstScene/first.js
@@ -3,7 +3,7 @@ import * as THREE from "../node_modules/three/build/three.module.js";
 const canvas = document.getElementById("canvas");
 
 const canvasSize = {
-  width: 800,
+  width: 900,
   height: 600,
 };
 
@@ -23,15 +23,36 @@ const camera = new THREE.PerspectiveCamera(
   cameraParams.far
 );
 
-camera.position.set(0, 1, 6);
+camera.position.set(0, 0, 30);
+camera.lookAt(0,0,0)
 
 scene.add(camera);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial();
-const cube = new THREE.Mesh(geometry, material);
+const geometry1 = new THREE.BoxGeometry(2, 1, 2);
+const material2 = new THREE.MeshBasicMaterial(
+  // { wireframe: true }
+  );
+
+const cube = new THREE.Mesh(geometry1, material2);
 
 scene.add(cube);
+
+
+const points = [];
+
+points.push(new THREE.Vector3(-10, -10, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, -10, 0));
+points.push(new THREE.Vector3(-10, -10, 0));
+
+
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+const material = new THREE.LineBasicMaterial({ color: "yellow" });
+
+
+const line = new THREE.Line(geometry, material);
+
+scene.add(line);
 
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 
